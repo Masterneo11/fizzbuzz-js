26b6fa0295975f13f8059f866c0d262fb6aa191f
Mathew Zamacona <mateoszamacona@icloud.com>
2024-06-04 17:20:19 -0600

    add event listener & syntax fix

--- fizz.js
+++ fizz.js
@@ -1,22 +1,22 @@
-
+document.getElementById("fizz").addEventListener("click", (e) => {
 
 function fizzBuzz(){
 
-    
-    
     for ( let i = 0; i < 101; i++){
-        if (i % 5 === 0 && i % 3 === 0){
+        if (i % 5 === 0 && i % 3 === 0) {
             console.log(`${i}: Fizz Buzz`);
         }
-        else if( i % 5 === 0){
+        else if ( i % 5 === 0) {
             console.log(`${i}: Buzz`);
         }
-        else if(i % 3 === 0){
+        else if (i % 3 === 0) {
             console.log(`${i}: Fizz`);
         }
         else {
             console.log(i);
         }
     }
-    alert(`fizz buzz wooo `)
+    alert(`fizz buzz wooo `);
 }
+fizzBuzz();
+});
